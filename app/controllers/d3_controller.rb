df592af67e1c24e9e2ec3afc2aa8829115978903
prusswan <prusswan@gmail.com>
2018-06-19 15:54:05 +0800

    fixes to work with 2e example structure

--- app/controllers/d3_controller.rb
+++ app/controllers/d3_controller.rb
@@ -2,15 +2,23 @@ class D3Controller < ApplicationController
   prepend_view_path Rails.root
 
   def index
-    @files = Dir.glob("*.html").sort!
-    params[:figure] ||= @files.first.chomp('.html')
+    @files = Dir.glob("chapter*/*.html").sort
+    if params[:figure]
+      if params[:figure].starts_with?("chapter")
+        params[:chapter] = /chapter(?<c>\d+)/.match(params[:figure])['c'].to_i
+      end
+    else
+      params[:figure] = 'index'
+    end
 
     respond_to do |format|
       format.html do
         result = render(template: "#{params[:figure]}.html", layout: !(false || params[:raw]))
         result = result.gsub!('d3.v3.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js') || result
         result = result.gsub!('.select("body")', '.select(".col")') || result
-        result = result.gsub!(/(src=\")(?!\S*http|\/)([^"\s]+\")/, '\1https://cdn.rawgit.com/prusswan/d3ia/0715b2dd/\2') || result
+        # result = result.gsub!(/(src=\")(?!\S*http|\/)([^"\s]+\")/, '\1https://cdn.rawgit.com/prusswan/d3ia/0715b2dd/\2') || result
+        @chapter = "/chapter#{params[:chapter]}" if params[:chapter]
+        result = result.gsub!(/(src=\")(?!\S*http|\/)([^"\s]+\")/, '\1https://cdn.rawgit.com/emeeks/d3_in_action_2/b1be4f52%{c}/\2' % {c: @chapter}) || result
 
         return result
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'd3/*figure', to: 'd3#index'
+  get '*figure', to: 'd3#index'
 
   root to: 'd3#index'
 end
