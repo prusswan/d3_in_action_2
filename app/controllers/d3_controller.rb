bbba3b1106c3cb7f38aac0320bdacca72623c8eb
prusswan <prusswan@gmail.com>
2018-06-20 16:50:25 +0800

    added stylesheet_pack_tag (extract-text plugin collects css into separate file)

--- app/controllers/d3_controller.rb
+++ app/controllers/d3_controller.rb
@@ -2,7 +2,9 @@ class D3Controller < ApplicationController
   prepend_view_path Rails.root
 
   def index
-    @files = Dir.glob("chapter*/*.html").sort
+    @files = Dir.glob("chapter*/*.html*").sort
+    @files = @files.map{|f| f.gsub('.erb', '') }
+
     if params[:figure]
       if params[:figure].starts_with?("chapter")
         params[:chapter] = /chapter(?<c>\d+)/.match(params[:figure])['c'].to_i
@@ -15,7 +17,7 @@ class D3Controller < ApplicationController
       format.html do
         result = render(template: "#{params[:figure]}.html", layout: !(false || params[:raw]))
         result = result.gsub!('d3.v3.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js') || result
-        result = result.gsub!('.select("body")', '.select(".col")') || result
+        result = result.gsub!('.select("body")', '.select(".col-sm-9")') || result
         # result = result.gsub!(/(src=\")(?!\S*http|\/)([^"\s]+\")/, '\1https://cdn.rawgit.com/prusswan/d3ia/0715b2dd/\2') || result
         @chapter = "/chapter#{params[:chapter]}" if params[:chapter]
         result = result.gsub!(/(src=\")(?!\S*http|\/)([^"\s]+\")/, '\1https://cdn.rawgit.com/emeeks/d3_in_action_2/b1be4f52%{c}/\2' % {c: @chapter}) || result
