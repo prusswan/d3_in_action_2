566f47c9460e728762955620a94228b366b74d62
prusswan <prusswan@gmail.com>
2018-06-20 13:27:06 +0800

    fixed import css issue by upgrading react-scripts (to 1.x) and postcss-loader (to 2.x)

--- chapter9/reactd3/src/App.js
+++ chapter9/reactd3/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-//import './App.css'
+import './App.css'
 import WorldMap from './WorldMap'
 import BarChart from './BarChart'
 import StreamGraph from './StreamGraph'
