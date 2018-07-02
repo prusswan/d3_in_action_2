a28bdcbebc98a0e1dd0128fa8c156c5e0c23ec29
prusswan <prusswan@gmail.com>
2018-07-02 17:55:01 +0800

    fixed build issue with d3-transition by prepending Rails.root.node_modules

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
+const { resolve } = require('path')
 
+const basePath = resolve(".");
+
+// environment.config.set('resolve.modules', basePath + '/node_modules')
+// environment.config.set('resolve.mainFields', ['module', 'main']) //, 'main', 'browser'])
+
+environment.resolvedModules.prepend('root', basePath + '/node_modules')
 module.exports = environment
+
+console.log("basePath", basePath);
+console.log("webpack config", environment);
